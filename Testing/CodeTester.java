0df1c0df651dcadd0c4cfca7a286f58e40ad1035
Giuseppe Turini <turinig@gmail.com>
2022-03-29 13:33:16 -0400

    Initial commit.

---
+++ Testing/CodeTester.java
@@ -0,0 +1,59 @@
+
+
+
+// CS-102: "Computing and Algorithms I"
+// CS-102: "Computing and Algorithms II"
+// CS-203: "Computing and Algorithms III"
+// Prof. Giuseppe Turini
+// Kettering University
+// 2022-03-29
+
+package Testing;
+
+import Recursion.*;
+
+import DataStructures.List.ArrayList;
+import DataStructures.List.LinkedList;
+
+import Algorithms.DecreaseAndConquer.*;
+
+//import MyList.MyLinkedList;
+//import MyBST.MyBST;
+
+//import DataStructure.Tree.BST.BSTReferenceBased;
+//import DataStructure.Tree.BST.BSTException;
+
+//import MyHashtable.MyHashtable;
+
+// ...
+public class CodeTester { 
+
+   // ...
+   public static void main( String[] args ) throws Exception {
+   
+      int[] A = { 10, 20, 30, 40, 0, 50, 60, 70, 80, 90 };
+      
+      ArrayList al = new ArrayList();
+      LinkedList ll = new LinkedList();
+      
+      Algorithms.DecreaseAndConquer.Sorting.InsertionSort(A);
+      
+      int choice = 3;
+      switch (choice) {
+         case 1 -> {}
+         case 2 -> {}}
+         
+      // trims the trailing and leading spaces
+      String s = " geeks for geeks has all java functions to read  ";
+      System.out.println(s.trim());
+      // trims the leading spaces
+      s = " Chetna loves reading books";
+      System.out.println(s.trim());
+      
+      // ...
+      System.out.println("END MAIN");
+   }
+   
+}
+
+
