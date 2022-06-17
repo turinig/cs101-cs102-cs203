2d812152879da3d100b0e9c8f4578806e9c0eb6d
Giuseppe Turini <turinig@gmail.com>
2022-06-17 08:41:57 -0400

    Initial commit.

---
+++ DataStructures/Tree/BST/BSTRuntimeException.java
@@ -0,0 +1,24 @@
+
+
+
+// CS-102: "Computing and Algorithms II"
+// CS-203: "Computing and Algorithms III"
+// Prof. Giuseppe Turini
+// Kettering University
+// 2022-06-17
+
+package DataStructures.Tree.BST;
+
+import java.lang.RuntimeException;
+
+// Exception (non-critical) for runtime errors of BST operations.
+public class BSTRuntimeException extends RuntimeException {
+
+   // Constructor.
+   public BSTRuntimeException( String s ) { 
+      super(s);
+   }
+
+}
+
+
