f09c2784ab2e9000af091caaafc351439510d8f4
Giuseppe Turini <turinig@gmail.com>
2022-11-14 19:07:00 -0500

    Initial commit

---
+++ DataStructures/Tree/AVL/AVLException.java
@@ -0,0 +1,23 @@
+
+
+
+// CS-203: "Computing and Algorithms III"
+// Prof. Giuseppe Turini
+// Kettering University
+// 2022-11-14
+
+package DataStructures.Tree.AVL;
+
+import java.lang.Exception;
+
+// Exception (critical) for runtime errors of AVL operations.
+public class AVLException extends Exception {
+
+   // Constructor.
+   public AVLException( String s ) { 
+      super(s);
+   }
+
+}
+
+
---
+++ DataStructures/Tree/AVL/AVLRuntimeException.java
@@ -0,0 +1,23 @@
+
+
+
+// CS-203: "Computing and Algorithms III"
+// Prof. Giuseppe Turini
+// Kettering University
+// 2022-11-14
+
+package DataStructures.Tree.AVL;
+
+import java.lang.RuntimeException;
+
+// Exception (non-critical) for runtime errors of AVL operations.
+public class AVLRuntimeException extends RuntimeException {
+
+   // Constructor.
+   public AVLRuntimeException( String s ) { 
+      super(s);
+   }
+
+}
+
+
